f1c859b58b4002fbc944f5bf8f445bdb60bd36e3
Andy Ybarra <andy.ybarra@gatech.edu>
2016-04-04 03:10:17 -0400

    Updated workflow for the cardviews

--- app/src/main/java/com/gtpd/k9/k9record/Explosive.java
+++ app/src/main/java/com/gtpd/k9/k9record/Explosive.java
@@ -1,5 +1,8 @@
 package com.gtpd.k9.k9record;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+
 public class Explosive {
 
     public String name;
@@ -7,6 +10,8 @@ public class Explosive {
     public Unit unit;
     public String location;
     public int imageResource;
+    public Timestamp startTime;
+    public Timestamp endTime;
 
     public Explosive(String name, int imageResource) {
         this.name = name;
@@ -27,5 +32,33 @@ public class Explosive {
 
     public enum Unit {
         KG, G, LB, OZ
+    }
+
+    public void setStartTime(Timestamp start) {
+        startTime = start;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setEndTime(Timestamp end){
+        endTime = end;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public String getEllapsedTime() {
+        long diff = endTime.getTime() - startTime.getTime();
+        long seconds = diff / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+//        long days = hours / 24;
+        String hour_str = (hours < 10) ? "0" + hours: "" + hours;
+        String minutes_str = (minutes < 10) ? "0" + minutes: "" + minutes;
+        String seconds_str = (seconds < 10) ? "0" + seconds: "" + seconds;
+        return "" + hour_str + ":" + minutes_str + ":" + seconds_str;
     }
 }
